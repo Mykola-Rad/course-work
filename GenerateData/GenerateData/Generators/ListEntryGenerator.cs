b9964e36142ea978c6251415eeb767911ac4d699
Mykola-Rad <radmykola1789@gmail.com>
2025-04-30 13:32:32 +0300

    implemented list entry generation logic

--- GenerateData/GenerateData/Generators/ListEntryGenerator.cs
+++ GenerateData/GenerateData/Generators/ListEntryGenerator.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Bogus;
+using GenerateData.Models;
 
 namespace GenerateData.Generators
 {
-    internal class ListEntryGenerator
+    public class ListEntryGenerator : IEntityGenerator<ListEntry>
     {
+        private const int minCount = 1;
+        private const int maxCount = 100;
+        private const decimal minPrice = 10;
+        private const decimal maxPrice = 1000;
+        public List<ListEntry> Generate(int count, GenerationContext context)
+        {
+            var uniqueEntries = new HashSet<(string productName, int invoice_id)>();
+            var listEntries = new List<ListEntry>();
+
+            var faker = new Faker<ListEntry>()
+                .RuleFor(le => le.InvoiceId, f => f.PickRandom(context.AvailableInvoiceIds))
+                .RuleFor(le => le.ProductName, f => f.PickRandom(context.AvailableProductNames))
+                .RuleFor(le => le.Count, f => Math.Round(f.Random.Decimal(minCount, maxCount), 2))
+                .RuleFor(le => le.Price, f => Math.Round(f.Random.Decimal(minPrice, maxPrice), 2));
+
+            while (listEntries.Count < count)
+            {
+                var entry = faker.Generate();
+
+                var combination = (entry.ProductName, entry.InvoiceId);
+                if (uniqueEntries.Add(combination))
+                    listEntries.Add(entry);
+            }
+
+            return listEntries;
+        }
     }
 }
