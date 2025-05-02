9a146a8d262c79beb6b59ca7894e0fab04cf6018
Mykola-Rad <radmykola1789@gmail.com>
2025-05-02 16:58:09 +0300

    finished base products management

--- GenerateData/IMS/Models/Product.cs
+++ GenerateData/IMS/Models/Product.cs
@@ -1,19 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace IMS.Models;
 
 public partial class Product
 {
+    [Key] 
+    [Required(ErrorMessage = "Назва товару є обов'язковою.")]
+    [StringLength(100, ErrorMessage = "Назва товару не може перевищувати 100 символів.")]
+    [Display(Name = "Назва товару")]
     public string ProductName { get; set; } = null!;
 
+    [Required(ErrorMessage = "Одиниця виміру є обов'язковою.")]
+    [StringLength(10)]
+    [Display(Name = "Код од. виміру")]
     public string UnitCode { get; set; } = null!;
 
+    [Required(ErrorMessage = "Ціна є обов'язковою.")]
+    [Range(0.01, 99999999.99, ErrorMessage = "Ціна повинна бути позитивним числом.")] 
+    [Display(Name = "Остання ціна")]
     public decimal LastPrice { get; set; }
 
     public virtual ICollection<ListEntry> ListEntries { get; set; } = new List<ListEntry>();
 
     public virtual ICollection<StorageProduct> StorageProducts { get; set; } = new List<StorageProduct>();
 
+    [ForeignKey("UnitCode")] 
+    [Display(Name = "Одиниця виміру")]
     public virtual ProductUnit UnitCodeNavigation { get; set; } = null!;
 }
--- GenerateData/IMS/Models/ProductUnit.cs
+++ GenerateData/IMS/Models/ProductUnit.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace IMS.Models;
 
 public partial class ProductUnit
 {
+    [Key]
+    [Required]
+    [StringLength(10)]
     public string UnitCode { get; set; } = null!;
 
+    [Required]
+    [StringLength(50)]
     public string UnitName { get; set; } = null!;
 
     public virtual ICollection<Product> Products { get; set; } = new List<Product>();
