0216836d1b2d571a0fbd32c0c36f6e743692d9cc
Mykola-Rad <radmykola1789@gmail.com>
2025-05-03 16:45:27 +0300

    finished units management logic

--- GenerateData/IMS/Models/ProductUnit.cs
+++ GenerateData/IMS/Models/ProductUnit.cs
@@ -7,12 +7,14 @@ namespace IMS.Models;
 public partial class ProductUnit
 {
     [Key]
-    [Required]
-    [StringLength(10)]
+    [Required(ErrorMessage = "Код одиниці виміру є обов'язковим.")]
+    [StringLength(10, ErrorMessage = "Код одиниці виміру не може перевищувати 10 символів.")]
+    [Display(Name = "Код одиниці")]
     public string UnitCode { get; set; } = null!;
 
-    [Required]
-    [StringLength(50)]
+    [Required(ErrorMessage = "Назва одиниці виміру є обов'язковою.")]
+    [StringLength(50, ErrorMessage = "Назва одиниці виміру не може перевищувати 50 символів.")]
+    [Display(Name = "Назва одиниці")]
     public string UnitName { get; set; } = null!;
 
     public virtual ICollection<Product> Products { get; set; } = new List<Product>();
