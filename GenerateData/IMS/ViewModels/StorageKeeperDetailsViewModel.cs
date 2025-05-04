0e5ce1d40a9252939e64bbb05b3d2d6bc86d371b
Mykola-Rad <radmykola1789@gmail.com>
2025-05-04 13:36:53 +0300

    added base storage keeper management logic

---
+++ GenerateData/IMS/ViewModels/StorageKeeperDetailsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using IMS.Models;
+
+namespace IMS.ViewModels
+{
+    public class StorageKeeperDetailsViewModel
+    {
+        public StorageKeeper Keeper { get; set; } = null!;
+
+        public List<Invoice> RelatedInvoices { get; set; } = new List<Invoice>();
+
+        public bool ShowInvoices { get; set; }
+    }
+}
